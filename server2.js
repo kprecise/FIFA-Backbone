a91ccd33d65df8c31cc2964bc5b1e53aae2b947d
david.huang <david.huang@blastradius.com>
2012-11-15 14:23:34 -0500

    Server updates.

--- server2.js
+++ server2.js
@@ -12,7 +12,22 @@ var application_root = __dirname,
 
 var app = express.createServer();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 app.configure(function () {
+	app.use(allowCrossDomain);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   //app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
